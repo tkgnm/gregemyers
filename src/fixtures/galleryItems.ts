240da06db44fbc33c3b6caf416aa7774b6fdf6e2
Thomas King <102960825+tkgnm@users.noreply.github.com>
2023-01-18 23:06:42 +0000

    removed gregemyers from each caption

--- src/fixtures/galleryItems.ts
+++ src/fixtures/galleryItems.ts
@@ -4,7 +4,7 @@ export const galleryItems: GalleryItem[] = [
   {
     id: "1",
     title: "Earth portals 2.0",
-    caption: "Chem 70x59cm Shelter 48x48cm ©Gregemyers2023",
+    caption: "Chem 70x59cm Shelter 48x48cm",
     coverImageURL: "https://gem.blipdiscs.com/img/gallerythumbnails/1.jpeg",
     content: "My example content",
   },
@@ -12,7 +12,7 @@ export const galleryItems: GalleryItem[] = [
     id: "2",
     title: "Zoom paradise",
     caption:
-      "Zoom Paradise 🏝 46x36cm - Digital Print Custom fade box frame by myself ©Gregemyers2022",
+      "Zoom Paradise 🏝 46x36cm - Digital Print Custom fade box frame by myself",
     coverImageURL: "https://gem.blipdiscs.com/img/gallerythumbnails/2.jpeg",
     content: "My second content",
   },
@@ -20,7 +20,7 @@ export const galleryItems: GalleryItem[] = [
     id: "3",
     title: "Earth portals",
     caption:
-      "Roof (I) 43x54cm Roof (II) 46x54cm White Curl 60x60cm Roof (I) 43x54cm Flat Roof 60x60cm ©Gregemyers2022",
+      "Roof (I) 43x54cm Roof (II) 46x54cm White Curl 60x60cm Roof (I) 43x54cm Flat Roof 60x60cm",
     coverImageURL: "https://gem.blipdiscs.com/img/gallerythumbnails/3.jpeg",
     content: "My third content",
   },
@@ -28,7 +28,7 @@ export const galleryItems: GalleryItem[] = [
     id: "4",
     title: "Content Filtering L to R",
     caption:
-      "Digital Prints on Paper Photos from a collaborative show with Harriotte Hodson ©Gregemyers2022",
+      "Digital Prints on Paper Photos from a collaborative show with Harriotte Hodson",
     coverImageURL: "https://gem.blipdiscs.com/img/gallerythumbnails/4.jpeg",
     content: "My example content",
   },
@@ -36,29 +36,28 @@ export const galleryItems: GalleryItem[] = [
     id: "5",
     title: "I'm Here & Everywhere (Morning & Night)",
     caption:
-      "A forthcoming short film based on a series of photographs related to my filtered online self. ©Gregemyers2022",
+      "A forthcoming short film based on a series of photographs related to my filtered online self.",
     coverImageURL: "https://gem.blipdiscs.com/img/gallerythumbnails/5.jpeg",
     content: "My second content",
   },
   {
     id: "6",
     title: "Shades of Blue Archive (Jpgs)",
-    caption: "A Series Of 13 Digital Prints on Paper ©Gregemyers2022",
+    caption: "A Series Of 13 Digital Prints on Paper",
     coverImageURL: "https://gem.blipdiscs.com/img/gallerythumbnails/6.jpeg",
     content: "My third content",
   },
   {
     id: "7",
     title: "Scenario (Colour)",
-    caption: "A Series of shorts coming Soon ©Gregemyers2022",
+    caption: "A Series of shorts coming Soon",
     coverImageURL: "https://gem.blipdiscs.com/img/gallerythumbnails/7.jpeg",
     content: "My example content",
   },
   {
     id: "8",
     title: "Home Sweet Home",
-    caption:
-      "A Series of 24 Photographs Photocopies & Cardboard Boxes ©Gregemyers2022",
+    caption: "A Series of 24 Photographs Photocopies & Cardboard Boxes",
     coverImageURL: "https://gem.blipdiscs.com/img/gallerythumbnails/8.jpeg",
     content: "My second content",
   },
