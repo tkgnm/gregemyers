9030b376880d65c9e2115671eb9d9507eae5ac2b
Thomas King <102960825+tkgnm@users.noreply.github.com>
2023-01-22 20:14:59 +0000

    added underline to titles, added contact title

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,7 @@
 export const Contact = () => {
   return (
     <div style={`max-width: 768px; margin:0 auto;`}>
+      <h2>Contact</h2>
       <p> Please enquire for sales, commissions and gallery shows. </p>
       Email: gregemyers92@gmail.com <br />
       Instagram: <a href="instagram.com/gregemyers">instagram.com/gregemyers</a>
