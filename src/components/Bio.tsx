b50ba38f93d5cf05a581f7cd18eb291c060be271
Thomas King <102960825+tkgnm@users.noreply.github.com>
2023-06-02 22:56:48 +0100

    Revert "server routing"

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,19 +1,15 @@
-import "../app.css";
-import { render } from "preact";
-import { Banner } from "../components/Banner";
-import { NavBar } from "../components/NavBar";
-import { Gallery } from "../components/Gallery";
+import { Divider } from "./Divider";
 import { useState, useEffect } from "preact/hooks";
 
 export const Bio = () => {
   const [html, setHtml] = useState("Loading...");
-  let BEARER_TOKEN = import.meta.env.VITE_BEARER_TOKEN;
+
   useEffect(() => {
     fetch("https://gregemyers-api-fly.fly.dev/api/bio", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${BEARER_TOKEN}`,
+        Authorization: `Bearer ${import.meta.env.VITE_BEARER_TOKEN}`,
       },
     })
       .then((res) => res.json())
@@ -25,14 +21,10 @@ export const Bio = () => {
 
   return (
     <>
-      <Banner />
-      <NavBar />
       <div style={`max-width: 768px; margin:0 auto;`}>
         <div dangerouslySetInnerHTML={{ __html: html }} />
       </div>
-      <Gallery />
-      <br />
-      All content on this site ©️ Greg E Myers
+      <Divider />
     </>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
 import { render } from "preact";
 import { App } from "./app";
 
-render(<App />, document.getElementById("root") as HTMLElement);
+render(<App />, document.getElementById("app") as HTMLElement);
