bd1a6d43aa3cd77dae7b4ae775e160a5b844ba8c
Thomas King <102960825+tkgnm@users.noreply.github.com>
2023-06-06 21:58:21 +0100

    made target blank

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,7 +8,9 @@ export const Contact = () => {
         <p> Please enquire for sales, commissions and gallery shows. </p>
         Email: contact@gregemyers.com <br />
         Instagram:{" "}
-        <a href="https://www.instagram.com/gregemyers">@gregemyers</a>
+        <a href="https://www.instagram.com/gregemyers" target="_blank">
+          @gregemyers
+        </a>
         <br />
         <br />
       </div>
